e1edf8bed99b10894029b2bea937d6f4cd7ab666
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-10 21:00:27 +0100

    creo el tablero y las fichas

--- ajedrez.py
+++ ajedrez.py
@@ -1,1 +1,26 @@
 from random import randint, sample
+tablero =[
+    [' ','1','2','3','4','5','6','7','8'],
+    ['1',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['2',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['3',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['4',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['5',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['6',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['7',' ',' ',' ',' ',' ',' ',' ',' '],
+    ['8',' ',' ',' ',' ',' ',' ',' ',' '],
+]
+
+REY_BLANCO = chr(0x2654)
+REINA_BLANCO = chr(0x2655)
+TORRE_BLANCO = chr(0x2656)
+ALFIL_BLANCO = chr(0x2657)
+CABALLO_BLANCO = chr(0x2658)
+PEÓN_BLANCO = chr(0x2659)
+
+REY_NEGRO = chr(0x265A)
+REINA_NEGRO = chr(0x265B)
+TORRE_NEGRO = chr(0x265C)
+ALFIL_NEGRO = chr(0x265D)
+CABALLO_NEGRO = chr(0x265E)
+PEÓN_NEGRO = chr(0x265F)
